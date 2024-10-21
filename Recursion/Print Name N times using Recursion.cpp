004280d152e1fe31fe4e23ca7f3dc17d2ddb14b0
Mariom Emu <69960457+MariomEmu@users.noreply.github.com>
2024-10-21 10:13:27 +0600

    Update Print Name N times using Recursion.cpp

--- Recursion/Print Name N times using Recursion.cpp
+++ Recursion/Print Name N times using Recursion.cpp
@@ -21,3 +21,28 @@ int main()
    func(1,n);
     return 0;
 }
+
+//////////////////////////////////////////////////////////////////////
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ void f(int n,int cnt = 0){  ///parameterized way recursion
+
+ if(cnt == n) return;
+
+ cout<<"emu"<<endl;
+
+ f(n,cnt+1);
+ }
+int main()
+{
+    int n;
+    cin>>n;
+
+    f(n);
+    return 0;
+}
