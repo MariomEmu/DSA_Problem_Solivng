cba13ff5c95248dce1a8d5de7715cadf506914b1
Mariom Emu <69960457+MariomEmu@users.noreply.github.com>
2024-11-13 13:44:04 +0600

    Add files via upload

---
+++ Recursion/Recursion  basic 16 problem solve/GCD using Recursion.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int gcd( int a, int b)
+{
+    if( b == 0)
+    {
+         return a;
+    }
+
+    return gcd( b, a%b);
+}
+
+int main()
+{
+   int n1,n2;
+   cin>>n1>>n2;
+
+   int GCD = gcd(n1,n2);
+   cout<<GCD<<endl;
+
+
+
+
+    return 0;
+}
